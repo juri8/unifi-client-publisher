6bf31b60bd7f6104076e949e203617c6b8ce624d
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2020-04-01 21:16:17 +0200

    initial version loading list of known clients

---
+++ README.md
@@ -0,0 +1,7 @@
+# Unifi Client Publisher
+This application will get the clients connected to a unifi controller with the ability to re-publishe them
+somewhere else (e.g. mqtt). The purpose is to be able to use this in home-automatjion projects.
+
+# Sources
+
+[Unifi Controller API documentation](https://ubntwiki.com/products/software/unifi-controller/api)
---
+++ unifi-client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import yaml
+import requests
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+config = cfg["default"]
+url = config["url"]
+api = url + "/api/s/" + config["site"]
+
+s = requests.session()
+
+
+def login():
+    auth = {"username": config["user"], "password": config["passwd"]}
+    return s.post(url + "/api/login", json=auth, verify=False)
+
+
+def logout():
+    return s.get(url + "/api/logout", verify=False)
+
+
+def get_clients():
+    return s.get(api + "/rest/user", verify=False)
+
+
+login()
+# TODO: verify success
+r = get_clients()
+clients = r.json()
+logout()
+
+print(clients["data"])
