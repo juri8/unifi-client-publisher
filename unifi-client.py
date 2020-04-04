4bb95747d3e37e90839507c3943a89f77de40ab6
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2020-04-04 17:25:52 +0200

    add mqtt publishing

--- unifi-client.py
+++ unifi-client.py
@@ -11,15 +11,23 @@ def get_mqtt(config):
     if("user" in config):
         client.username_pw_set(config['user'], config['passwd'])
     client.connect(config['host'])
+    return client
+
 
 unifi = Unifi.login(cfg['unifi'])
 r = unifi.get_clients()
 clients = r.json()
 unifi.logout()
 
-# mqtt_client = get_mqtt(cfg['mqtt'])
-# mqtt_client.publish()
+mqtt_config = cfg['mqtt']
+mqtt_client = get_mqtt(mqtt_config)
 
+baseTopic = mqtt_config['baseTopic']
 
 for entry in clients['data']:
-    print(entry)
+    # print(entry)
+    device_topic = baseTopic + entry['mac'] + "/"
+    mqtt_client.publish(device_topic + "name", entry['hostname'])
+    mqtt_client.publish(device_topic + "last_seen", entry['last_seen'])
+
+mqtt_client.disconnect()
