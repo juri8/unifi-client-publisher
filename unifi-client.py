a2ddaea03f8bfa119e2cee5f4bdc3fb0e91c047d
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2020-04-04 11:34:36 +0200

    add mqtt publishing

--- unifi-client.py
+++ unifi-client.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 import yaml
 from Unifi import Unifi
+import paho.mqtt.client as mqtt
 
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-unifi = Unifi.login(cfg['unifi'])
+def get_mqtt(config):
+    client = mqtt.Client()
+    if("user" in config):
+        client.username_pw_set(config['user'], config['passwd'])
+    client.connect(config['host'])
 
+unifi = Unifi.login(cfg['unifi'])
 r = unifi.get_clients()
 clients = r.json()
 unifi.logout()
 
-print(clients["data"])
+# mqtt_client = get_mqtt(cfg['mqtt'])
+# mqtt_client.publish()
+
+
+for entry in clients['data']:
+    print(entry)
