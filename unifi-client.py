e9f8a447a774aaa33e09408e9e9acd767124341c
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2020-04-07 20:29:12 +0200

    use publish.multiple

--- unifi-client.py
+++ unifi-client.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
 import yaml
 from Unifi import Unifi
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
-
-def get_mqtt(config):
-    client = mqtt.Client()
-    if("user" in config):
-        client.username_pw_set(config['user'], config['passwd'])
-    client.connect(config['host'])
-    return client
-
 
 unifi = Unifi.login(cfg['unifi'])
 r = unifi.get_clients()
@@ -20,14 +12,17 @@ clients = r.json()
 unifi.logout()
 
 mqtt_config = cfg['mqtt']
-mqtt_client = get_mqtt(mqtt_config)
 
 baseTopic = mqtt_config['baseTopic']
-
+msgs = []
 for entry in clients['data']:
     # print(entry)
     device_topic = baseTopic + entry['mac'] + "/"
-    mqtt_client.publish(device_topic + "name", entry['hostname'])
-    mqtt_client.publish(device_topic + "last_seen", entry['last_seen'])
+    msgs.append((device_topic + "name", entry['hostname']))
+    msgs.append((device_topic + "last_seen", entry['last_seen']))
 
-mqtt_client.disconnect()
+mqtt_auth = {
+    'username': mqtt_config['user'],
+    'password': mqtt_config['passwd']
+}
+publish.multiple(msgs, mqtt_config['host'], auth = mqtt_auth)
